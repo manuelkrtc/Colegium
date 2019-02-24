5bed50c22bf1fe26959f346ac04254aae0ab5d52
mpaez <mapez@muvobike.com>
2019-02-23 20:13:14 -0500

    Feat(Articles): The fragment articles are created

--- appAndroid/app/src/main/java/com/example/colegium/MainActivity.java
+++ appAndroid/app/src/main/java/com/example/colegium/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.colegium;
 
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+
+import com.example.colegium.entities.articles.ArticlesView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ViewGroup fragment;
+
+    //Fragment
+    int refFragment = R.id.fragment;
+    FragmentTransaction fragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragment = findViewById(refFragment);
+
+        goFragmentArticles();
+    }
+
+    //--------------------------------------------- goFragments ------------------------------------
+
+    private void goFragmentArticles() {
+
+        ArticlesView fragment = ArticlesView.newInstance();
+
+        fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.replace(refFragment, fragment);
+        fragmentTransaction.addToBackStack(fragment.NAME);
+        fragmentTransaction.commit();
     }
 }
---
+++ appAndroid/app/src/main/java/com/example/colegium/entities/articles/ArticlesView.java
@@ -0,0 +1,38 @@
+package com.example.colegium.entities.articles;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.colegium.R;
+
+public class ArticlesView extends Fragment {
+
+    public static final String NAME = "ArticlesView";
+
+    public static ArticlesView newInstance() {
+        ArticlesView fragment = new ArticlesView();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_articles, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
